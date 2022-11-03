f14c9f3f791789776be68fb73ed011a678d4db00
github.com/nathanparente <nathan.vieira@raccoon.ag>
2022-11-03 11:52:47 -0300

    Adicionando funcionalidade ao Modal

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function openModal() {
+  const modal = document.getElementById("modal");
+  modal.classList.add("enabled");
+}
+
+function closeModal() {
+  const modal = document.getElementById("modal");
+  modal.classList.remove("enabled");
+}
