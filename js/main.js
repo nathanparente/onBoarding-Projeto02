6e2ddd9b8d772258663fd756acd235c20fd99374
github.com/nathanparente <nathan.vieira@raccoon.ag>
2022-11-17 09:37:40 -0300

    Ajustando responsividade accordionArea

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 function openModal() {
   const modal = document.getElementById("modal");
+  //const main = document.getElementById("main");
   modal.classList.add("enabled");
+  //main.classList.add("disabled");
 }
 
 function closeModal() {
   const modal = document.getElementById("modal");
   modal.classList.remove("enabled");
 }
+
+// function toggleModal() {
+//   const modal = document.getElementById("modal");
+// }
